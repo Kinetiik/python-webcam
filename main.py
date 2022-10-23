2a39cf3e97279e75a159169b4a8d6a3b8856bbab
Fynn <f.madrian18230@hs-ansbach.de>
2022-10-23 12:36:19 +0200

    first full version, need to remove flickering

--- main.py
+++ main.py
@@ -22,6 +22,32 @@ def increase_visibility(img):
     thresh = cv2.threshold(L, value, 255, cv2.THRESH_BINARY)[1]
 
     # invert threshold and make 3 channels
+    #thresh = 255 - thresh
+    thresh = cv2.merge([thresh, thresh, thresh])
+
+    gain = 0.75
+    blue = cv2.multiply(img[:, :, 0], gain)
+    green = cv2.multiply(img[:, :, 1], gain)
+    red = cv2.multiply(img[:, :, 2], gain)
+    img_bright = cv2.merge([blue, green, red])
+
+    # blend original and brightened using thresh as mask
+    img = np.where(thresh == 255, img_bright, img)
+
+    LAB = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+
+    L = LAB[:, :, 0]
+
+    # threshold L channel with triangle method
+    value, thresh = cv2.threshold(
+        L, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_TRIANGLE)
+    print(value)
+
+    # threshold with adjusted value
+    value = value + 10
+    thresh = cv2.threshold(L, value, 255, cv2.THRESH_BINARY)[1]
+
+    # invert threshold and make 3 channels
     thresh = 255 - thresh
     thresh = cv2.merge([thresh, thresh, thresh])
 
